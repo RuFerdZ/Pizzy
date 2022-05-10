36c131580b6a1a1ecd06481d87fc4716becd74dc
RuFerdZ <ge.rusiru@gmail.com>
2022-05-11 06:44:55 +0800

    Initial Commit

---
+++ PizzaApplication/PizzaApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaApplicationApp.swift
+//  PizzaApplication
+//
+//  Created by user215753 on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PizzaApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
